39a379d534d307c483ba0541f1b856cab76c407e
Elliott Fries <fries.15@wright.edu>
2019-06-19 12:41:50 -0400

    Project files imported

---
+++ Assets/goal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goal : MonoBehaviour {
+
+	public GameObject winText;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.tag == "Player") {
+			winText.SetActive (true);
+			StartCoroutine (wait ());
+		}
+	}
+
+	IEnumerator wait() {
+		
+		yield return new WaitForSeconds (5);
+	}
+}
---
+++ Assets/throwHook.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class throwHook : MonoBehaviour {
+
+
+	public GameObject hook;
+
+
+	public bool ropeActive;
+
+	GameObject curHook;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+
+		if (Input.GetMouseButtonDown (0)) {
+
+			if (ropeActive == false) {
+				
+				Vector2 destiny = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+				curHook = (GameObject)Instantiate (hook, transform.position, Quaternion.identity);
+
+				curHook.GetComponent<RopeScript> ().destiny = destiny;
+
+				ropeActive = true;
+			} else {
+
+				Destroy (curHook);
+
+				ropeActive = false;
+
+			}
+
+		}
+
+
+	}
+}
